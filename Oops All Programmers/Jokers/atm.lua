24ecd5a53ef235aad78ab73a63c7f6f1849367f7
the-Astra <djs8743@rit.edu>
2026-01-11 00:07:06 -0500

    Fix ATM case-sensitive access caused by new smods

--- Oops All Programmers/Jokers/atm.lua
+++ Oops All Programmers/Jokers/atm.lua
@@ -11,18 +11,18 @@ SMODS.Joker {
         }
     },
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.money, card.ability.extra.money * (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.Czech or 0) } }
+        return { vars = { card.ability.extra.money, card.ability.extra.money * (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.czech or 0) } }
     end,
     calculate = function(self, card, context)
         if context.using_consumeable and not context.blueprint and context.consumeable.ability.set == "Czech" then
             return {
-                message = '+' .. localize('$') .. G.GAME.consumeable_usage_total.Czech,
+                message = '+' .. localize('$') .. G.GAME.consumeable_usage_total.czech,
                 colour = G.C.MONEY
             }
         end
     end,
     calc_dollar_bonus = function(self, card)
-        local uses = (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.Czech or 0)
+        local uses = (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.czech or 0)
         return to_number(uses) > 0 and (card.ability.extra.money * uses) or nil
 
     end,
