f3ee67d4372f783753da4947fcd1246809d15cd2
the-Astra <djs8743@rit.edu>
2026-01-24 19:30:48 -0500

    Mega Mushroom destroys itself properly now

--- Oops All Programmers/Jokers/mega_mushroom.lua
+++ Oops All Programmers/Jokers/mega_mushroom.lua
@@ -32,27 +32,8 @@ SMODS.Joker {
                 level_up = 2
             }
         end
-        if context.after and not context.blueprint
-            and card.ability.extra.hands_left <= 0 then
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    play_sound('tarot1')
-                    card.T.r = -0.2
-                    card:juice_up(0.3, 0.4)
-                    card.states.drag.is = true
-                    card.children.center.pinch.x = true
-                    G.E_MANAGER:add_event(Event({
-                        trigger = 'after',
-                        delay = 0.3,
-                        blockable = false,
-                        func = function()
-                            card:remove()
-                            return true
-                        end
-                    }))
-                    return true
-                end
-            }))
+        if context.after and not context.blueprint and card.ability.extra.hands_left <= 0 then
+            SMODS.destroy_cards(card, nil, nil, true)
             return {
                 message = localize('k_eaten_ex'),
                 colour = G.C.FILTER
