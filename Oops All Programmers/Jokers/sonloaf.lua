6db25979497f34c7db611533db6f4f425f910a10
the-Astra <djs8743@rit.edu>
2026-01-24 19:34:31 -0500

    Fix Son Loaf not using a comparison (how did this even happen)

--- Oops All Programmers/Jokers/sonloaf.lua
+++ Oops All Programmers/Jokers/sonloaf.lua
@@ -11,7 +11,7 @@ SMODS.Joker {
         team = { 'O!AP' }
     },
     calculate = function(self, card, context)
-        if context.joker_type_destroyed and context.card.config.center.pools and context.card.config.center.pools.Food and G.jokers.config.card_limit - (#G.jokers.cards + G.GAME.joker_buffer) then
+        if context.joker_type_destroyed and context.card.config.center.pools and context.card.config.center.pools.Food and G.jokers.config.card_limit > #G.jokers.cards + G.GAME.joker_buffer then
             G.GAME.joker_buffer = G.GAME.joker_buffer + 1
             G.E_MANAGER:add_event(Event({
                 func = function()
